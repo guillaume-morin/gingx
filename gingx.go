2bbb6025f75912d62485839fd811abeb4d86defb
Guillaume Morin <gmorin@sysif.net>
2015-07-09 09:19:19 +0200

    Add file detection

--- gingx.go
+++ gingx.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	//"strings"
 	//"path/filepath"
 )
@@ -17,6 +18,11 @@ func http_handler(w http.ResponseWriter, r *http.Request) {
 	document_root := "/home/gmorin/git/gingx"
 	target_file := fmt.Sprint(document_root, r.URL.Path)
 	fmt.Fprintln(w, fmt.Sprint("Requested file : ", target_file))
+	if file_stat, err := os.Stat(target_file); err == nil {
+		fmt.Fprintln(w, file_stat)
+	} else {
+		fmt.Fprintln(w, "404 File not found")
+	}
 	// FileMode
 	//files, _ := filepath.Glob("*")
 	//fmt.Fprintln(w, files)
